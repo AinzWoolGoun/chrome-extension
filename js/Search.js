f10f73b0e219bbb7afb5bdfab1f02b4dda7bf914
AinzWoolGoun <747890647@qq.com>
2018-11-18 12:19:31 +0800

    add Google Translate

--- js/Search.js
+++ js/Search.js
@@ -5,3 +5,11 @@ chrome.contextMenus.create({
     chrome.tabs.create({url:"http://www.baidu.com/s?ie=utf-8&wd="+encodeURI(params.selectionText)})
   }
 });
+
+chrome.contextMenus.create({
+  title:"使用Google翻译\"%s\"",
+  contexts:['selection'],
+  onclick:function(params){
+    chrome.tabs.create({url:"https://translate.google.cn/#en/zh-CN/"+encodeURI(params.selectionText)})
+  }
+});
